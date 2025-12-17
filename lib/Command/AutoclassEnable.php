0591b4eaea9a016b7a967d763273352f1db8c0dd
Kent Delante <kent.delante@proton.me>
2025-12-17 16:06:15 +0800

    feat: enable autoclass for buckets when the command is run
    Signed-off-by: Kent Delante <kent.delante@proton.me>

--- lib/Command/AutoclassEnable.php
+++ lib/Command/AutoclassEnable.php
@@ -8,15 +8,18 @@ declare(strict_types=1);
 namespace OCA\FilesGCS\Command;
 
 use OCA\FilesGCS\Config;
+use OCA\FilesGCS\Services\AutoclassService;
+use OCP\IConfig;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AutoclassEnable extends Command {
 
 	public function __construct(
-		private Config $config
+		private Config $appConfig,
+		private IConfig $config,
+		private AutoclassService $service,
 	) {
 		parent::__construct();
 	}
@@ -27,8 +30,37 @@ class AutoclassEnable extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
-		$this->config->setAutoclassEnabled(true);
-		$output->writeln('Autoclass enabled');
+		$this->appConfig->setAutoclassEnabled(true);
+		$output->writeln('New buckets will be created with autoclass enabled');
+
+		$objectstore = $this->config->getSystemValue('objectstore');
+		if (!is_array($objectstore)) {
+			return Command::SUCCESS;
+		}
+
+		if (strpos($objectstore['arguments']['hostname'], 'storage.googleapis.com') === false) {
+			return Command::SUCCESS;
+		}
+
+		$bucketName = $objectstore['arguments']['bucket'];
+		if (empty($bucketName)) {
+			return Command::SUCCESS;
+		}
+
+		$output->writeln('Detected bucket "' . $bucketName . '", enabling autoclass...');
+		$credentials = $this->appConfig->getCredentials();
+		if (empty($credentials)) {
+			$output->writeln('Credentials not setup, skipping bucket update');
+			return Command::SUCCESS;
+		}
+
+		$enabled = $this->service->enable($bucketName, $credentials);
+		if (!$enabled) {
+			$output->writeln('<error>Failed to enable autoclass for bucket ' . $bucketName . '</error>');
+			return Command::FAILURE;
+		}
+
+		$output->writeln('<info>Autoclass for bucket ' . $bucketName . ' enabled</info>');
 
 		return Command::SUCCESS;
 	}
---
+++ lib/Services/AutoclassService.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+namespace OCA\FilesGCS\Services;
+
+use Google\Auth\Credentials\ServiceAccountCredentials;
+use Google\Cloud\Storage\StorageClient;
+use OCA\FilesGCS\Config;
+use Psr\Log\LoggerInterface;
+
+class AutoclassService {
+	public function __construct(
+		private Config $config,
+		private LoggerInterface $logger,
+	) {
+	}
+
+	public function enable(string $bucketName, ?string $credentials = null): bool {
+		if (!$credentials) {
+			$credentials = $this->config->getCredentials();
+		}
+
+		$credentialsFetcher = new ServiceAccountCredentials(StorageClient::FULL_CONTROL_SCOPE, json_decode($credentials, true));
+		$storage = new StorageClient(['credentialsFetcher' => $credentialsFetcher]);
+		$bucket = $storage->bucket($bucketName);
+
+		try {
+			$response = $bucket->update([
+				'autoclass' => [
+					'enabled' => true,
+					'terminalStorageClass' => strtoupper($this->config->getTerminalStorageClass())
+				]
+			]);
+		} catch (\Exception $e) {
+			$this->logger->error('Failed to enable autoclass for bucket ' . $bucketName, ['exception' => $e]);
+			return false;
+		}
+
+		if (empty($response['autoclass']['enabled'])) {
+			return false;
+		}
+
+		return true;
+	}
+}
