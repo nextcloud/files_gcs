e4240579b764d75ed869bf09352a6e79967b5309
Kent Delante <kent.delante@proton.me>
2026-01-28 20:57:55 +0800

    feat: add options to specify object store and bucket
    Signed-off-by: Kent Delante <kent@delante.me>

--- lib/Command/AutoclassEnable.php
+++ lib/Command/AutoclassEnable.php
@@ -9,9 +9,11 @@ namespace OCA\FilesGCS\Command;
 
 use OCA\FilesGCS\Config;
 use OCA\FilesGCS\Services\AutoclassService;
+use OCA\FilesGCS\Exceptions\BucketMissingException;
 use OCP\IConfig;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AutoclassEnable extends Command {
@@ -26,35 +28,50 @@ class AutoclassEnable extends Command {
 
 	protected function configure(): void {
 		$this->setName('files_gcs:autoclass:enable')
-			->setDescription('Enable autoclass for new buckets');
+			->setDescription('Enable autoclass for new buckets. If the objectstore and bucket are specified, an '
+							 . ' attempt to enable autoclass for the bucket will be initiated.')
+			->addOption('object-store', 'o', InputOption::VALUE_REQUIRED , 'The name of the objectstore')
+			->addOption('bucket', 'b', InputOption::VALUE_REQUIRED, 'The name of the bucket');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
 		$this->appConfig->setAutoclassEnabled(true);
 		$output->writeln('New buckets will be created with autoclass enabled');
 
-		$objectstore = $this->config->getSystemValue('objectstore');
-		if (!is_array($objectstore)) {
+		if (!($objectstoreName = $input->getOption('object-store'))) {
 			return Command::SUCCESS;
 		}
 
+		$objectstores = $this->config->getSystemValue('objectstore');
+		if (!isset($objectstores[$objectstoreName])) {
+			$output->writeln('<comment>No configuration found for object store ' . $objectstoreName . '</comment>');
+			return Command::SUCCESS;
+		}
+
+		if (!($bucketName = $input->getOption('bucket'))) {
+			return Command::SUCCESS;
+		}
+
+		$objectstore = $objectstores[$objectstoreName];
 		if (strpos($objectstore['arguments']['hostname'], 'storage.googleapis.com') === false) {
 			return Command::SUCCESS;
 		}
 
-		$bucketName = $objectstore['arguments']['bucket'];
-		if (empty($bucketName)) {
-			return Command::SUCCESS;
-		}
-
-		$output->writeln('Detected bucket "' . $bucketName . '", enabling autoclass...');
 		$credentials = $this->appConfig->getCredentials();
 		if (empty($credentials)) {
-			$output->writeln('Credentials not setup, skipping bucket update');
+			$output->writeln('<comment>Credentials not setup, skipping bucket update</comment>');
 			return Command::SUCCESS;
 		}
 
-		$enabled = $this->service->enable($bucketName, $credentials);
+		$output->writeln('Enabling autloclass for ' . $bucketName . '...');
+
+		try {
+			$enabled = $this->service->enable($bucketName, $credentials);
+		} catch (BucketMissingException $exception) {
+			$output->writeln('<comment>Bucket ' . $bucketName . ' does not yet exist. Autoclass will be enabled when it is created</comment>');
+			return Command::SUCCESS;
+		}
+
 		if (!$enabled) {
 			$output->writeln('<error>Failed to enable autoclass for bucket ' . $bucketName . '</error>');
 			return Command::FAILURE;
