6364e55f3fa775347713e38097fe92fd1e25fc7d
Kent Delante <kent@delante.me>
2026-08-22 21:06:16 +0800

    feat: setup config lexicon
    Signed-off-by: Kent Delante <kent@delante.me>

---
+++ lib/ConfigLexicon.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\FilesGCS;
+
+use OCP\Config\Lexicon\Entry;
+use OCP\Config\Lexicon\ILexicon;
+use OCP\Config\Lexicon\Strictness;
+use OCP\Config\ValueType;
+
+class ConfigLexicon implements ILexicon {
+	public const AUTOCLASS_ENABLED = 'autoclass_enabled';
+	public const TERMINAL_STORAGE_CLASS = 'terminal_storage_class';
+	public const CREDENTIALS = 'credentials';
+
+	#[\Override]
+	public function getStrictness(): Strictness {
+		return Strictness::EXCEPTION;
+	}
+
+	#[\Override]
+	public function getAppConfigs(): array {
+		return [
+			new Entry(
+				self::AUTOCLASS_ENABLED,
+				ValueType::BOOL,
+				false,
+				'Whether the autoclass setting is enabled on Google Cloud Storage',
+			),
+			new Entry(
+				self::TERMINAL_STORAGE_CLASS,
+				ValueType::STRING,
+				'Nearline',
+				'The storage class to be set when autoclass is enabled. Valid values are \'Nearline\' and \'Archive\''
+			),
+			new Entry(
+				self::CREDENTIALS,
+				ValueType::STRING
+			),
+		];
+	}
+
+	#[\Override]
+	public function getUserConfigs(): array {
+		return [];
+	}
+}
